6930a958d0f8e77f125987aad1086f39a149999f
Shubham Patrick <ahthneeuhl@gmail.com>
2023-07-10 00:11:28 +0530

    feat: handle search event in Search component

--- ui/src/components/Search.tsx
+++ ui/src/components/Search.tsx
@@ -1,14 +1,28 @@
+import { useState } from "react";
 import { Button, Col, Container, Form, Row } from "react-bootstrap";
 
 const Search: React.FC = () => {
+  const [word, setWord] = useState("");
+
+  const handleSearch = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    console.log(word);
+  };
+
   return (
     <Container className="mt-4">
       <Row className="justify-content-center">
         <Col xs={12} md={8}>
-          <Form>
+          <Form onSubmit={handleSearch}>
             <Row>
               <Col xs={9}>
-                <Form.Control placeholder="Search for images" />
+                <Form.Control
+                  type="text"
+                  value={word}
+                  onChange={(e) => setWord(e.target.value)}
+                  placeholder="Search for images"
+                />
               </Col>
               <Col>
                 <Button color="primary" type="submit">
