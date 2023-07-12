48ea934ff890e7476451aa21192a045ce8c8ab70
Shubham Patrick <ahthneeuhl@gmail.com>
2023-07-12 12:53:12 +0530

    feat: make new-image api endpoint

--- api/main.py
+++ api/main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -6,6 +6,12 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello World!"
+
+
+@app.route("/new-image")
+def new_image():
+    word = request.args.get("query")
+    return {"word": word}
 
 
 if __name__ == "__main__":
